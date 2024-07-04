961e70356f3d6f8360d19e4bbff26804658d0f03
Pratoosh-18 <pratoosh10garg@gmail.com>
2024-07-05 00:56:35 +0530

    Changed UI

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,21 @@ function App() {
   const [data, setData] = useState([]);
   const [user, setuser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isErrorInApi, setIsErrorInApi] = useState(false);
 
   useEffect(() => {
     const fetchAPI = async () => {
-      const response = await axios.get(
-        "https://602e7c2c4410730017c50b9d.mockapi.io/users"
-      );
-      const data = response.data;
-      setData(data);
-      console.log(data);
-      setIsLoading(true);
+      try {
+        const response = await axios.get(
+          "https://602e7c2c4410730017c50b9d.mockapi.io/users"
+        );
+        const data = response.data;
+        setData(data);
+        console.log(data);
+        setIsLoading(true);
+      } catch (error) {
+        setIsErrorInApi(true);
+      }
     };
     fetchAPI();
   }, []);
@@ -26,6 +31,19 @@ function App() {
 
   return (
     <>
+      {/* Error handling if API does not work */}
+      {isErrorInApi ? (
+        <div className="h-100vh w-100vw flex justify-center items-center text-2xl font-semibold flex-col">
+          <p>No data to show !</p>
+          <p>There might be some error in loading the data</p>
+          <p>Try reloading</p>
+        </div>
+      ) : (
+        <></>
+      )}
+
+      {/* Showing Loading if loading else load the data */}
+
       {isLoading ? (
         <>
           <div className="flex md:flex-row flex-col-reverse h-[100vh] w-[100%] overflow-hidden">
@@ -35,6 +53,7 @@ function App() {
                 data.map((item, index) => (
                   <div key={index}>
                     <div
+                    // highlighting the selected user and mapping them
                       onClick={() => handleUsernameClick(item)}
                       className={`cursor-pointer p-3 m-2 rounded-md ${
                         user && user.id === item.id
@@ -54,41 +73,56 @@ function App() {
                   </div>
                 ))}
             </div>
-            <div className="flex h-full w-full md:w-[60%] justify-center items-center ">
+            <div className="flex bg-yellow-100 h-full w-full md:w-[60%] justify-center items-center ">
               {/* <div className="UserCard w-[80%] flex justify-center items-center"> */}
               {user ? (
-                <div className="UserCard rounded-lg my-3 w-[350px] md:w-[400px] h-[350px] md:h-[500px] flex flex-col justify-center  border-2 border-black">
+
+                // Creating the user card
+
+                <div className="UserCard bg-white rounded-lg my-3 w-[350px] md:w-[400px] h-[350px] md:h-[500px] flex flex-col justify-center  border-2 border-black">
                   {/* <h2>User Details</h2> */}
                   <div className="flex w-full h-[170px] justify-center items-center">
                     <img
-                      className="h-[100px] w-[100px] md:h-[200px] md:w-[200px]"
+                      className="h-[100px] w-[100px] mb-2 md:h-[200px] md:w-[200px]"
                       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV1Mly7C6D_WWpPXTAO4dF52D9Wd9FKuC9zw&s"
                       alt=""
                     />
                   </div>
-                  <div className="h-[225px] flex flex-col justify-center text-sm md:text-base px-5">
+                  <div className="h-[225px] flex flex-col justify-center text-sm md:text-base px-5 mt-2">
                     <p>
-                      <span className="text-base md:text-lg font-medium">First name : </span>
+                      <span className="text-base md:text-lg font-medium">
+                        First name :{" "}
+                      </span>
                       {user.profile.firstName}
                     </p>
                     <p>
-                      <span className="text-base md:text-lg font-medium">Last name : </span>
+                      <span className="text-base md:text-lg font-medium">
+                        Last name :{" "}
+                      </span>
                       {user.profile.lastName}
                     </p>
                     <p>
-                      <span className="text-base md:text-lg font-medium">User name : </span>
+                      <span className="text-base md:text-lg font-medium">
+                        User name :{" "}
+                      </span>
                       {user.profile.username}
                     </p>
                     <p>
-                      <span className="text-base md:text-lg font-medium">Job title : </span>
-                       {user.jobTitle}
+                      <span className="text-base md:text-lg font-medium">
+                        Job title :{" "}
+                      </span>
+                      {user.jobTitle}
                     </p>
                     <p>
-                      <span className="text-base md:text-lg font-medium">Bio : </span>
+                      <span className="text-base md:text-lg font-medium">
+                        Bio :{" "}
+                      </span>
                       {user.Bio}
                     </p>
                     <p>
-                      <span className="text-base md:text-lg font-medium">Email : </span>
+                      <span className="text-base md:text-lg font-medium">
+                        Email :{" "}
+                      </span>
                       {user.profile.email.toLowerCase()}
                     </p>
                   </div>
@@ -101,6 +135,9 @@ function App() {
           </div>
         </>
       ) : (
+
+        // Loading animation logic
+
         <div className="w-full h-[100vh] flex justify-center items-center">
           <img
             class="w-20 h-20 animate-spin"
